22ecf81dbfb2856ec22bb5392fd68cce1df30cdd
pavan kulkarni <kulkarnipavan333@gmail.com>
2024-12-26 19:00:11 +0530

    java1

---
+++ Test.java
@@ -0,0 +1,15 @@
+package pkg2;
+
+public class Test {
+
+	public static void main(String[] args) {
+		 Tools t1=new Tools();
+		 t1.Numbers(10);
+		 t1.Odd(10);
+		 t1.Even(10);
+		 t1.Mult20(10);
+		  
+
+	}
+
+}
---
+++ Tools.java
@@ -0,0 +1,44 @@
+package pkg2;
+
+public class Tools{
+	public void Numbers(int n1) {
+		for(int i=0;i<=n1;i++) {
+			System.out.print(1*i+" ");
+			}
+			System.out.println("");
+	}
+		
+		 
+			
+	public void Odd(int n1) {
+		
+		for(int i=0;i<=n1;i++) {
+			System.out.print(2*i+1+" ");
+		}
+		System.out.println("");
+	}
+	
+	public void Even(int n1) {
+		for(int i=0;i<=n1;i++) {
+			System.out.print(2*i+" ");
+			 
+		}
+		System.out.println("");
+	}
+	
+	
+	public void Mult20(int n1) {
+		for( int j=3;j<21;j++) {
+			for(int i=0;i<=n1;i++) {
+				System.out.print(j*i+" ");
+		}
+			System.out.println("");
+		 
+	}
+	}
+}
+	
+	
+	
+	
+	 
